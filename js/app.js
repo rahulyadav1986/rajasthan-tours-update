06f66703f7e3f96fb4d26cc44da9882ba446916a
rahulyadav1986 <rahullooksforward@gmail.com>
2022-12-08 23:25:23 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -48,29 +48,27 @@ jQuery('#testi-carousel').owlCarousel({
     }
 });
 
-
-
-// Add minus icon for collapse element which is open by default
-jQuery(".collapse.show").each(function(){
-    jQuery(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
-  });
-  
-  // Toggle plus minus icon on show hide of collapse element
-  jQuery(".collapse").on('show.bs.collapse', function(){
-    jQuery(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
-  }).on('hide.bs.collapse', function(){
-    jQuery(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
-  });
-
-
-  jQuery(".card .card-header:first").addClass("active").show(); //Activate first tab
-
-  jQuery(".card-header").on('click', '.card-header', function(){    
-    if (jQuery(this).find('.accordion-toggle').hasClass('active')) {        
+jQuery('#desti-carousel').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:false,
+	dots:true,	
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
     }
-    
+});
 
- })
+
+
+
 
  $('[data-fancybox="gallery"]').fancybox({
 	// Options will go here
